42cc58f06ede4c4136b4d67ad332ba09059d49cd
eosipovproj <itfytd@ya.ru>
2022-10-19 18:53:27 +0300

    изменены классы

--- sandbox/src/main/java/ru/stqa/java_course/sandbox/App.java
+++ sandbox/src/main/java/ru/stqa/java_course/sandbox/App.java
@@ -5,16 +5,10 @@ public class App{
 		System.out.println("Hello, world!");
 		Square s = new Square(5);
 //		s.l = 5;
-		System.out.println("площадь квадрата со стороной " + s.l + " = " + area(s));
+		System.out.println("площадь квадрата со стороной " + s.l + " = " + s.area());
 		Rectangle r = new Rectangle(5, 7);
 //		r.a = 5;
 //		r.b = 7;
-		System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b +  " = " + area(r));
-	}
-	public static double area(Square s) {
-		return s.l * s.l;
-	}
-	public static double area(Rectangle r) {
-		return r.a * r.b;
+		System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b +  " = " + r.area());
 	}
 }
