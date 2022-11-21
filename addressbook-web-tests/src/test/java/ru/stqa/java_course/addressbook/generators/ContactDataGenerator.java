49700702e5236bd83b264144191e66ad96dfc479
eosipovproj <itfytd@ya.ru>
2022-11-21 22:24:53 +0300

    лекция

--- addressbook-web-tests/src/test/java/ru/stqa/java_course/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_course/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.java_course.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.java_course.addressbook.model.ContactData;
 
 import java.io.File;
@@ -10,15 +13,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ContactDataGenerator {
-    public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-        List<ContactData> contacts = genetateContacts(count);
-        save(contacts, file);
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
 
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static void save(List<ContactData> contacts, File file) throws IOException {
+    private void run() throws IOException {
+        List<ContactData> contacts = genetateContacts(count);
+        save(contacts, new File(file));
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for(ContactData contact : contacts){
             writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
@@ -28,7 +45,7 @@ public class ContactDataGenerator {
         writer.close();
     }
 
-    private static List<ContactData> genetateContacts(int count) {
+    private List<ContactData> genetateContacts(int count) {
         List<ContactData> contacts = new ArrayList<ContactData>();
         for (int i = 0; i < count; i++){
             contacts.add(new ContactData().withFirstname(String.format("name %s", i)).withLastname(String.format("lastname %s", i))
