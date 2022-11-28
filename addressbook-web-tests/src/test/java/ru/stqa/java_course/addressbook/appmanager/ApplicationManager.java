5d2f961c8cefe6342c4c1b27ddb71b5bb37921e0
eosipovproj <itfytd@ya.ru>
2022-11-28 19:57:51 +0300

    s

--- addressbook-web-tests/src/test/java/ru/stqa/java_course/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_course/addressbook/appmanager/ApplicationManager.java
@@ -30,7 +30,7 @@ public class ApplicationManager {
 
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
-        properties.load(new FileReader(new File(String.format("src/test/resources/$s.properties", target))));
+        properties.load(new FileReader(new File(String.format("src/test/resources/local.properties", target))));
         if (browser.equals(Browser.CHROME.browserName())) {
             wd = new ChromeDriver();
         } else if (browser.equals(Browser.IE.browserName())) {
