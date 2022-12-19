327fac4b1a98d23e0c8935656639fb8a891f657e
eosipovproj <itfytd@ya.ru>
2022-12-20 00:35:56 +0300

    code edit

--- addressbook-web-tests/src/test/java/ru/stqa/java_course/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_course/addressbook/appmanager/ApplicationManager.java
@@ -34,7 +34,7 @@ public class ApplicationManager {
 
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
-        properties.load(new FileReader(new File(String.format("src/test/resources/local.properties", target))));
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
         dbHelper = new DbHelper();
         if ("".equals(properties.getProperty("selenium.server"))) {
             if (browser.equals(Browser.CHROME.browserName())) {
